2ee2d068de7935ff07bd1b53ee47c76fbe0364aa
MikeyKanyungo <mikekanyungo95@gmail>
2023-08-30 11:48:15 +0200

    added project 10 - milestone (:

---
+++ project10 - wandaContacts/README.txt.txt
@@ -0,0 +1,6 @@
+This Python program is a basic phone book manager that allows users to input and manage a list of names and corresponding phone numbers. 
+Users can enter the number of entries they want to add, and then input the name and phone number for each entry. 
+The program displays the entered data in a tabular format with columns for names and phone numbers. 
+Users can also search for a specific name in the phone book, and the program will display the corresponding phone number if the name is found. 
+The program stores the data in a dictionary and provides an option to save the data to a JSON file for future use. 
+This simple utility helps users keep track of contact information in a user-friendly manner.
---
+++ project10 - wandaContacts/index.py
@@ -0,0 +1,33 @@
+#we will use this json module for exporting to a json file. 
+import json 
+
+# names and phone numbers will be saved to this phone_book dictionary
+phone_book = {}
+
+# enter the number of phone numbers a user wants to save
+num = int(input("Entries: "))
+
+#this iterates asking for inputs num times
+for i in range(num):
+	name = input("Name: ")
+	phone_number = int(input("Phone: "))
+	phone_book[name] = phone_number
+
+#this line saves data to a json file
+with open("phone_book.json", "w") as json_file:
+	json.dump(phone_book, json_file)
+
+print ("\nName\t\t\tPhone Number\n")
+
+for name, phone_number in phone_book.items():
+	print("{}\t\t\t".format(name, phone_number)) 
+
+search_term = input("\nEnter search term: ")
+
+print ("Search Result: ")
+if search_term in phone_book:
+	phone_number = phone_book[search_term]
+	print("Name: {}, Phone: {}".format(search_term, phone_number))
+	
+else:
+	print ("Name not found")
